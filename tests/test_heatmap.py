57b655c41bd07cd5df733f7ce7072aeae2124a77
Suyash <suyashkumar2409@github.com>
2023-11-10 20:47:39 -0500

    removed extra files

--- tests/test_heatmap.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from modules.data import HeatmapGenerator
-
-class TestHeatMap(unittest.TestCase):
-    def test_heatmap(self):
-        heat_map_generator = HeatmapGenerator({
-
-        })
